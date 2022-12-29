67da3b204341a5f8b921fbf2c8909c3550930bef
Hasanul Bari <hasanulbari8@gmail.com>
2022-12-29 12:55:51 +0600

    update

--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -3,19 +3,32 @@ import { useParams } from "react-router-dom";
 import axios from "axios";
 
 const UserDetails = () => {
-  const [user, setUsers] = useState([]);
+  const [user, setUser] = useState([]);
   const userId = useParams();
+
+  //   useEffect(() => {
+  //     fetch(`https://main.hangerstock.com/api/user/get/id`, {
+  //       method: "POST",
+  //       headers: {
+  //         "content-type": "application/json",
+  //       },
+  //       body: JSON.stringify({ id: userId.id }),
+  //     })
+  //       .then((res) => res.json())
+  //       .then((data) => setUser(data.data));
+  //   }, []);
 
   useEffect(() => {
     axios
-      .post(`https://main.hangerstock.com/api/user/get/${userId}`)
-      .then((data) => setUsers(data));
-  }, [userId]);
+      .post("https://main.hangerstock.com/api/user/get/id", {
+        id: userId.id,
+      })
+      .then((data) => setUser(data.data.data));
+  }, []);
 
-  console.log(user);
   return (
     <div>
-      <h2>Details</h2>
+      <h2>Email:{user?.email}</h2>
     </div>
   );
 };
